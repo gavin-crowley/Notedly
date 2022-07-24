aaa418dde68df4a3c4880026c77fe764e2b97a6c
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-07-24 08:23:21 +0100

    graphql hello world

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
+// Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World!!!!'));
+// Construct a schema, using GraphQL's schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+// Provide resolver functions for our schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+};
+const app = express();
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
-
-// app.listen(4000, () => console.log('Listening on port 4000!'))
