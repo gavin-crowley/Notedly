2b1a0193c7713c6903ce86d6217f3bfaae16e9f7
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-07-24 11:35:24 +0100

    delete and update added - full CRUD in API

--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -4,5 +4,32 @@ module.exports = {
       content: args.content,
       author: 'Adam Scott'
     });
+  },
+  deleteNote: async (parent, { id }, { models }) => {
+    try {
+      await models.Note.findOneAndRemove({ _id: id });
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+  updateNote: async (parent, { content, id }, { models }) => {
+    try {
+      return await models.Note.findOneAndUpdate(
+        {
+          _id: id
+        },
+        {
+          $set: {
+            content
+          }
+        },
+        {
+          new: true
+        }
+      );
+    } catch (err) {
+      throw new Error('Error updating note');
+    }
   }
 };
